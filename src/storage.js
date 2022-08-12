8a60697543dc7b9453904d7391173065e0402874
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-12 21:01:01 +0300

    create a class to handle local storage

---
+++ src/storage.js
@@ -0,0 +1,13 @@
+// JS to handle local storage
+export default class Storage {
+  static getLocalStorage() {
+    if ((localStorage.getItem('to-do-item')) === null || (localStorage.getItem('to-do-item')) === 'undefined') {
+      return [];
+    }
+    return JSON.parse(localStorage.getItem('to-do-item'));
+  }
+
+  static SetLocalStorage(arr) {
+    localStorage.setItem('to-do-item', JSON.stringify(arr));
+  }
+}
