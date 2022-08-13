a79e3b9a9c3b362fab17878f54ced2ac82c0d34e
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-13 13:16:54 +0300

    reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,34 @@
+/* eslint-disable linebreak-style */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'To-do-list',
       template: './src/index.html',
     }),
   ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
     ],
-  },
-
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
