03f76f389749c0e06589f0cc7921187f956b144b
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-13 13:57:40 +0300

    add interactivity module

---
+++ src/modules/interactive.js
@@ -0,0 +1,35 @@
+import { toDoListData } from './todo.js';
+import resetIndex from './resetIndex.js';
+import { setData } from './setItems.js';
+
+export function removeTask(reindex) {
+  const toDoListDatas = toDoListData().filter((todo) => todo.index !== reindex);
+  resetIndex(toDoListDatas);
+  setData(toDoListDatas);
+}
+
+export function editTodo(reindex, todoDescription) {
+  const toDoListDatas = toDoListData().map((todo) => {
+    if (todo.index === reindex) {
+      return { ...todo, description: todoDescription };
+    }
+    return todo;
+  });
+  setData(toDoListDatas);
+}
+
+export function completeTodo(reindex) {
+  const duplicate = toDoListData();
+  duplicate.forEach((element) => {
+    if (element.index === reindex) {
+      element.completed = !element.completed;
+    }
+  });
+  setData(duplicate);
+}
+
+export function clearCompletedTodos() {
+  const toDoListDatas = toDoListData().filter((todo) => !todo.completed);
+  resetIndex(toDoListDatas);
+  setData(toDoListDatas);
+}
